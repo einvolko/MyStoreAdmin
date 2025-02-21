a388de28fe78f632f09d8913f90cd1587a0cd852
einvolko <einvolko@mail.ru>
2025-02-21 16:16:19 +0300

    Initial Commit

---
+++ MyStoreAdmin/MyStoreAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoreAdminApp.swift
+//  MyStoreAdmin
+//
+//  Created by Михаил Супрун on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MyStoreAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyStoreAdminTests/MyStoreAdminTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyStoreAdminTests.swift
+//  MyStoreAdminTests
+//
+//  Created by Михаил Супрун on 2/21/25.
+//
+
+import Testing
+@testable import MyStoreAdmin
+
+struct MyStoreAdminTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
